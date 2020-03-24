7950ec5344e931d0eb3ba26d6be23879f7787133
Marcin Kubiak <hugo26@o2.pl>
2020-03-24 09:40:03 +0100

    add Accordion and styling ti Project Options

--- client/src/components/features/ProjectCard/ProjectCard.js
+++ client/src/components/features/ProjectCard/ProjectCard.js
@@ -6,9 +6,15 @@ const ProjectCard = ({ project }) => {
   const { name, githublink, deploylink, techstack, description, img } = project;
   return (
     <Col xs={12} sm={6} lg={4} className="section--card">
-      <Card border="success">
+      <Card>
         <Card.Header as="h5">{name}</Card.Header>
-        {img && <Card.Img variant="top" src={`/images/pages/${img}`} />}
+        {img && (
+          <Card.Img
+            variant="top"
+            src={`/images/pages/${img}`}
+            className="screenshot"
+          />
+        )}
         <Card.Body>
           <Card.Subtitle className="mb-2 text-muted">Description</Card.Subtitle>
           <Card.Text>{description}</Card.Text>
--- client/src/components/features/ProjectOptions/ProjectOptions.js
+++ client/src/components/features/ProjectOptions/ProjectOptions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Form } from 'react-bootstrap';
+import { Col, Form, Accordion, Button } from 'react-bootstrap';
 import './ProjectOptions.style.scss';
 import techStacks from '../../../data/techstacks';
 
@@ -21,46 +21,58 @@ const ProjectOptions = ({ filters, setFilters }) => {
 
   return (
     <Col lg={12} className="form--group">
-      <Form>
-        <Form.Row>
-          <Col xs={12} sm={6} lg={4} className="input--group">
-            <Form.Label>Project name</Form.Label>
-            <Form.Control
-              onChange={e => handleChange(e)}
-              value={name}
-              type="text"
-              placeholder="Enter part of project name"
-              name="title"
-            />
-          </Col>
-          <Col xs={12} sm={6} lg={4} className="input--group">
-            <Form.Label>Tech used:</Form.Label>
-            <div>
-              {techStacks.map(tech => (
-                <Form.Check
-                  inline
+      <Accordion>
+        <Accordion.Toggle
+          as={Button}
+          block
+          variant="outline-primary"
+          eventKey="0"
+        >
+          Sort options
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="0">
+          <Form>
+            <Form.Row>
+              <Col xs={12} sm={6} lg={4} className="input--group">
+                <Form.Label>Project name</Form.Label>
+                <Form.Control
                   onChange={e => handleChange(e)}
-                  type="checkbox"
-                  label={tech}
-                  name="tech"
-                  id={tech}
-                  key={tech}
+                  value={name}
+                  type="text"
+                  placeholder="Enter part of project name"
+                  name="title"
                 />
-              ))}
-            </div>
-          </Col>
-          <Col xs={12} sm={6} lg={4} className="input--group">
-            <Form.Label>Project deployed:</Form.Label>
-            <Form.Check
-              onChange={e => handleChange(e)}
-              type="switch"
-              id="check4"
-              label="Deployed"
-              name="deploy"
-            />
-          </Col>
-        </Form.Row>
-      </Form>
+              </Col>
+              <Col xs={12} sm={6} lg={4} className="input--group">
+                <Form.Label>Tech used:</Form.Label>
+                <div>
+                  {techStacks.map(tech => (
+                    <Form.Check
+                      inline
+                      onChange={e => handleChange(e)}
+                      type="checkbox"
+                      label={tech}
+                      name="tech"
+                      id={tech}
+                      key={tech}
+                    />
+                  ))}
+                </div>
+              </Col>
+              <Col xs={12} sm={6} lg={4} className="input--group">
+                <Form.Label>Project deployed:</Form.Label>
+                <Form.Check
+                  onChange={e => handleChange(e)}
+                  type="switch"
+                  id="check4"
+                  label="Deployed"
+                  name="deploy"
+                />
+              </Col>
+            </Form.Row>
+          </Form>
+        </Accordion.Collapse>
+      </Accordion>
     </Col>
   );
 };
