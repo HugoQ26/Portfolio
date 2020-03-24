96a0ee97d42d9fdea573353d18be82491709d585
Marcin Kubiak <hugo26@o2.pl>
2020-03-24 12:29:00 +0100

    add modal

--- client/src/components/features/ProjectCard/ProjectCard.js
+++ client/src/components/features/ProjectCard/ProjectCard.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Col, Modal } from 'react-bootstrap';
 import './ProjectCard.style.scss';
 
 const ProjectCard = ({ project }) => {
   const { name, githublink, deploylink, techstack, description, img } = project;
+  const [show, setShow] = useState(false);
   return (
     <Col xs={12} sm={6} lg={4} className="section--card">
       <Card>
@@ -13,6 +14,7 @@ const ProjectCard = ({ project }) => {
             variant="top"
             src={`/images/pages/${img}`}
             className="screenshot"
+            onClick={() => setShow(true)}
           />
         )}
         <Card.Body>
@@ -34,6 +36,23 @@ const ProjectCard = ({ project }) => {
             <img src={`/images/${tech}.png`} alt={`${tech} logo`} key={tech} />
           ))}
         </Card.Footer>
+        <Modal
+          show={show}
+          onHide={() => setShow(false)}
+          dialogClassName="modal-90w"
+          aria-labelledby="example-custom-modal-styling-title"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title as="h5">{name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img
+              src={`/images/pages/${img}`}
+              className="screenshot--modal"
+              onClick={() => setShow(true)}
+            />
+          </Modal.Body>
+        </Modal>
       </Card>
     </Col>
   );
